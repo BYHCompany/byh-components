094ab214f57e39b430c83097bd03195a005cd015
Artem <artemsimutin@gmail.com>
2021-05-29 02:35:58 +0300

    after success building with rollup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
