44c75bbc3afdd4506d67e8ad1dfe7d7c8bf023c2
quintui <rscrafter1234@icloud.com>
2021-05-29 18:50:15 +0300

    useless commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-typescript'],
 };
