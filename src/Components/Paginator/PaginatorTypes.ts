8ce63192f413e2dcb08427b0858945ab41b0b1cd
Artem <trystatik@icloud.com>
2021-06-06 00:22:31 +0300

    setted button Props in paginator as optional

--- src/Components/Paginator/PaginatorTypes.ts
+++ src/Components/Paginator/PaginatorTypes.ts
@@ -4,16 +4,16 @@ export interface PaginatorProps {
    * Summary pages count
    */
   allPagesCount: number;
-    /**
+  /**
    * Curent page index
    */
   currentPage: number;
-    /**
+  /**
    * Custom callback, gets (index: number) like arg
    */
   callback?: (index: number) => void;
   /**
    * Props for each button, props like in Button API
    */
-  buttonProps: ButtonProps
+  buttonProps?: ButtonProps;
 }
