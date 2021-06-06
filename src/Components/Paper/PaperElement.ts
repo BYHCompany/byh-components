ec13ac5ca5e1e18ee6ee58da1a89273d2e20fe33
Quintui <rscrafter1234@icloud.com>
2021-06-06 20:21:18 +0300

    fixed paper width

--- src/Components/Paper/PaperElement.ts
+++ src/Components/Paper/PaperElement.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import { PaperWrapperProps } from './paperType';
 
 export const PaperWrapper = styled.div<PaperWrapperProps>`
-  display: ${({ display }) => (display ? display : 'flex')};
-  height: ${({ height, maxHeight }) => (maxHeight && '100%') || (height ? `${height}px` : '100%')};
+  display: ${({ display }) => (display ? display : 'block')};
+  height: ${({ height, maxHeight }) => (maxHeight && '100%') || (height ? `${height}px` : 'auto')};
   width: ${({ width, maxWidth }) => (maxWidth && '100%') || (width ? `${width}px` : 'auto')};
   max-height: ${({ maxHeight }) => (maxHeight ? `${maxHeight}px` : null)};
   max-width: ${({ maxWidth }) => (maxWidth ? `${maxWidth}px` : null)};
   border-radius: ${({ borderRadius }) => borderRadius && borderRadius};
   box-shadow: ${({ boxShadow }) => (boxShadow ? '0px 4px 30px 0px rgba(0, 0, 0, 0.25)' : null)};
-  background: ${({ customBgColor }) => (customBgColor ? customBgColor : '#ebebeb')}; ;
+  background: ${({ customBgColor }) => (customBgColor ? customBgColor : '#ebebeb')};
 `;
