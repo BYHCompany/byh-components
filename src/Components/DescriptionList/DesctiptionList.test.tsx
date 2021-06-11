25a776ec756d9e8ddae639994fb17df0e5cbc28f
Quintui <rscrafter1234@icloud.com>
2021-06-11 11:12:14 +0300

    get rid of map in DecriptionList

--- src/Components/DescriptionList/DesctiptionList.test.tsx
+++ src/Components/DescriptionList/DesctiptionList.test.tsx
@@ -4,11 +4,8 @@ import '@testing-library/jest-dom/extend-expect';
 import { DescriptionList } from './DescriptionList';
 
 describe('DescriptionList', () => {
-  const obj = {
-    year: '2020',
-  };
   const getDescriptionList = () => {
-    return render(<DescriptionList width={300} obj={obj} />);
+    return render(<DescriptionList width={300} value="2020" key="Год" />);
   };
   it('DescriptionList should be rendered in the document', () => {
     const { getByTestId } = getDescriptionList();
