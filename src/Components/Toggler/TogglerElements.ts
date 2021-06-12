f5631671ceebf3f1ab1175fa7192ad92a18fabc6
Artem <artemsimutin@gmail.com>
2021-06-12 03:47:00 +0300

    NN21 Toggler is done without tests

--- src/Components/Toggler/TogglerElements.ts
+++ src/Components/Toggler/TogglerElements.ts
@@ -1,10 +1,39 @@
-import styled from 'styled-components';
-import { TogglerBackgroundTypes, TogglerSwitcherTypes } from './TogglerTypes';
+import styled, { css, keyframes } from 'styled-components';
+import { TogglerBackgroundTypes, TogglerSwitcherTypes, TogglerSwitcherWrapperTypes } from './TogglerTypes';
 
-export const TogglerWrapper = styled.div`
+export const moveRight = (x: number) => keyframes`
+  0% {
+    transform: none;
+  }
+  100% {
+    transform: translateX(${x}px);
+  }
+`
+
+export const moveLeft = (x: number) => keyframes`
+  0% {
+    transform: translateX(${x}px);
+  }
+  100% {
+    transform: none;
+  }
+`
+
+export const translateRight = (x: number) => css`
+  animation: ${moveRight(x)} 0.3s ease-in-out 1;
+  animation-fill-mode: forwards;
+`
+
+export const translateLeft = (x: number) => css`
+  animation: ${moveLeft(x)} 0.3s ease-in-out 1;
+  animation-fill-mode: forwards;
+`
+
+export const TogglerWrapper = styled.div<TogglerSwitcherWrapperTypes>`
   display: flex;
-  width: 80px;
-  height: 44px;
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+  align-items: center
 `;
 
 export const TogglerBackground = styled.label<TogglerBackgroundTypes>`
@@ -12,15 +41,16 @@ export const TogglerBackground = styled.label<TogglerBackgroundTypes>`
   background-color: ${({ isChecked }) => (isChecked ? '#072832' : '#ffffff')};
   width: 100%;
   height: 100%;
-  padding: 4px;
+  padding: ${({ padding }) => `${padding}px`};
   transition: all 0.2s ease-in-out;
 `;
 
 export const TogglerSwitcher = styled.div<TogglerSwitcherTypes>`
   animation-fill-mode: forwards;
-  height: 36px;
-  width: 36px;
+  height: ${({ heightAndWidth }) => `${heightAndWidth}px`};
+  width: ${({ heightAndWidth }) => `${heightAndWidth}px`};
   border-radius: 50px;
   background-color: ${({ isChecked }) => (isChecked ? `#ffffff` : `#072832`)};
   transition: all 0.2s ease-in-out;
+  ${({ move, distance }) => move === 'ŗight' && translateRight(distance) || move === 'left' && translateLeft(distance)}
 `;
--- src/Components/Toggler/TogglerTypes.ts
+++ src/Components/Toggler/TogglerTypes.ts
@@ -1,12 +1,24 @@
 export interface TogglerProps {
   value: boolean;
   onChange: any;
+  height?: number;
+  width?: number;
+  padding?: number
 }
 
-export interface TogglerSwitcherTypes extends React.ComponentPropsWithoutRef<'label'> {
+export interface TogglerSwitcherWrapperTypes extends React.ComponentPropsWithoutRef<'div'> {
+  width: number
+  height: number
+}
+
+export interface TogglerSwitcherTypes extends React.ComponentPropsWithoutRef<'div'> {
   isChecked: boolean;
+  heightAndWidth: number
+  move: 'left' | 'ŗight'
+  distance: number
 }
 
 export interface TogglerBackgroundTypes extends React.ComponentPropsWithoutRef<'div'> {
   isChecked: boolean;
+  padding: number
 }
