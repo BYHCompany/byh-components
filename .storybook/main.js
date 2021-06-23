621987480a0cc68bfb671259c8ee41c5f37d5600
Artem <artemsimutin@gmail.com>
2021-06-23 19:24:05 +0300

    deleted unnecessary deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,19 @@
+const postcss = require('postcss');
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-addon-react-docgen',
-    '@storybook/addon-postcss',
+    // 'storybook-addon-react-docgen',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: postcss,
+        },
+      },
+    },
   ],
 };
