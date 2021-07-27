d67da21b02da893956bb5d388b6ac6d977e5d1db
Quintui <rscrafter1234@icloud.com>
2021-07-27 18:20:32 +0300

    added animatio to accordion

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -4,9 +4,9 @@ import { Paper } from '../Paper/Paper';
 import { Title } from '../Title/Title';
 import { AccordionButton, ButtonContentWrapper, TitleWrapper } from './AccordionElements';
 import { AccordionProps } from './accordionTypes';
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 export const Accordion: React.FC<AccordionProps> = ({
-  title = "title",
+  title = 'title',
   children = <h1>Content</h1>,
   width = 770,
   titleSize = 20,
@@ -14,42 +14,47 @@ export const Accordion: React.FC<AccordionProps> = ({
 }) => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
   return (
-    <div style={{ width: width }} data-testid="accordion-wrapper">
-      <AccordionButton
-        height={height}
-        data-testid="accordion-button"
-        width={width}
-        isOpen={isOpen}
+    <AnimateSharedLayout>
+      <motion.div
+        initial={{ borderRadius: 3 }}
+        layout
         onClick={() => setIsOpen(!isOpen)}
-      >
-        <ButtonContentWrapper>
-          <TitleWrapper>
-            <h5 style={{ color: "#072832", fontSize: titleSize }}>{title}</h5>
-          </TitleWrapper>
-          {isOpen ? (
-            <IoIosArrowUp style={{ fontSize: 20 }} />
-          ) : (
-            <IoIosArrowDown style={{ fontSize: 20 }} />
-          )}
-        </ButtonContentWrapper>
-      </AccordionButton>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            layout
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            // boxShadow={false}
-            // borderRadius={"0 0 3px 3px"}
-            // customVariantColor={"#E9E9E9"}
-            // width={width}
-            style={{ padding: "15px 10px 15px 30px" }}
-          >
-            {children}
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+        style={{ width: width, backgroundColor: '#e9e9e9' }}
+        data-testid="accordion-wrapper"
+        transition={{ duration: 0.1 }}>
+        <motion.div layout transition={{ duration: 0.1 }}>
+          <AccordionButton
+            as={motion.button}
+            height={height}
+            data-testid="accordion-button"
+            width={width}
+            isOpen={isOpen}>
+            <ButtonContentWrapper as={motion.div} layout>
+              <TitleWrapper>
+                <h5 style={{ color: '#072832', fontSize: titleSize }}>{title}</h5>
+              </TitleWrapper>
+              {isOpen ? (
+                <IoIosArrowUp style={{ fontSize: 20 }} />
+              ) : (
+                <IoIosArrowDown style={{ fontSize: 20 }} />
+              )}
+            </ButtonContentWrapper>
+          </AccordionButton>
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                layout
+                transition={{ duration: 0.2 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                style={{ padding: '15px 10px 15px 30px' }}>
+                {children}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      </motion.div>
+    </AnimateSharedLayout>
   );
 };
